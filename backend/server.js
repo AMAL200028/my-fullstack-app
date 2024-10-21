4d450b202f7fab2cf79ce3ec0c9a990789e78f7e
Amal S B <amalcruzewaine@gmail.com>
2024-10-21 23:02:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 
 mongoose
-.connect(process.env.MONGO_URI,{
+.connect("mongodb+srv://amalcruzewaine:FOUfv6vmcnuIdmtk@cluster0.8ckv5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
     useNewUrlParser: true,
     UseUnifiedTopology: true,
 })
